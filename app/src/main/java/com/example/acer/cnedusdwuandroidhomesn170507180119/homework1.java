9efe2bd49aa61755461ebbfb57db72467bb35ea0
170507180119 <2916076379@qq.com>
2020-04-22 09:39:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/acer/cnedusdwuandroidhomesn170507180119/homework1.java
@@ -0,0 +1,12 @@
+package com.example.acer.cnedusdwuandroidhomesn170507180119;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class homework1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
